36f9b5bbf078d85444271a5b6cecd826824b1c21
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2022-10-13 21:48:06 -0400

    Add boot dirt particle to player prefab and disable on death

--- FarmerRun.Unity/Assets/PlayerDeath.cs
+++ FarmerRun.Unity/Assets/PlayerDeath.cs
@@ -5,7 +5,19 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class PlayerDeath : MonoBehaviour
 {
+    /// <summary>
+    /// The dirt particles kicked up by the player's boots
+    /// </summary>
+    [SerializeField]
+    private ParticleSystem mBootDirt;
+
+    /// <summary>
+    /// A reference to our game manager so we can keep track of whether or not the game is over
+    /// </summary>
     private GameManager mGameManager;
+    /// <summary>
+    /// A reference to the player's animator to be able to update the animator state on death
+    /// </summary>
     private Animator mAnimator;
 
     // Start is called before the first frame update
@@ -17,13 +29,19 @@ public class PlayerDeath : MonoBehaviour
         StartCoroutine(Die());
     }
 
+    /// <summary>
+    /// The function to run when the player dies, on game over
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator Die()
     {
         yield return new WaitUntil(() => this.mGameManager.IsGameOver);
+        this.mBootDirt.Stop();
         this.mAnimator.SetBool("Death_b", true);
         this.mAnimator.SetInteger("DeathType_int", 1);
+        
         //to move the player away from the fence
-        Vector3 displacement = -Vector3.forward * 0.5f;
+        Vector3 displacement = -Vector3.forward * 1.0f;
         this.transform.position += displacement;
     }
     
